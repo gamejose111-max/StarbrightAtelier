45ae9fc9d4a2d5ce35f0d45f992a4e569a27a916
José Games <gamejose111@gmail.com>
2026-03-01 16:11:36 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyDummyKey",
-  authDomain: "starbright-atelier.firebaseapp.com",
-  projectId: "starbright-atelier",
-  storageBucket: "starbright-atelier.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  "projectId": "studio-6330463347-ad38a",
+  "appId": "1:1012840501635:web:3e03ebc0836fb22c1826b6",
+  "apiKey": "AIzaSyCjEy6-PaPHTuqNIlTnyevmbILiZ6HoYVI",
+  "authDomain": "studio-6330463347-ad38a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1012840501635"
 };
